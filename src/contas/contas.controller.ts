e91dc16c15453974750c573f3df5560dc0f77152
JoaoBufon <jvbufon75@gmail.com>
2025-03-31 13:18:14 -0300

    feat: adicionado configuracao do swagger

--- src/contas/contas.controller.ts
+++ src/contas/contas.controller.ts
@@ -2,14 +2,15 @@ import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common'
 import { ContasService } from './contas.service';
 import { CreateContaDto } from './dto/create-conta.dto';
 import { UpdateContaDto } from './dto/update-conta.dto';
-    
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('Contas')
 @Controller('contas')
 export class ContasController {
   constructor(private readonly contasService: ContasService) {}
 
   @Post()
+  @ApiBody({ type: CreateContaDto })
   create(@Body() createContaDto: CreateContaDto) {
     return this.contasService.create(createContaDto);
   }
